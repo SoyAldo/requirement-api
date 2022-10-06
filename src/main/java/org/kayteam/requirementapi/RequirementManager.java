bea81a3d3f38f9df8a6737083db7062e96e1937b
SoyAldo <pereyraosvaldo@live.com.ar>
2022-10-06 16:41:51 -0300

    RequirementManager.java created.

---
+++ src/main/java/org/kayteam/requirementapi/RequirementManager.java
@@ -0,0 +1,124 @@
+package org.kayteam.requirementapi;
+
+import net.milkbowl.vault.economy.Economy;
+
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.plugin.RegisteredServiceProvider;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import org.kayteam.requirementapi.expansions.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RequirementManager {
+
+    private final JavaPlugin javaPlugin;
+    private Economy economy = null;
+    private final HashMap< String , Requirements > requirements = new HashMap<>();
+    private final HashMap< String , RequirementExpansion > requirementExpansions = new HashMap<>();
+
+    public RequirementManager(JavaPlugin javaPlugin) {
+        this.javaPlugin = javaPlugin;
+    }
+
+    public void registerManager() {
+
+        if (javaPlugin.getServer().getPluginManager().getPlugin("Vault") != null) {
+            RegisteredServiceProvider<Economy> registeredServiceProvider = javaPlugin.getServer().getServicesManager().getRegistration(Economy.class);
+            if (registeredServiceProvider != null) {
+                economy = registeredServiceProvider.getProvider();
+            }
+        }
+
+        registerExpansion( new HasExpExpansion() );
+        registerExpansion( new HasMoneyExpansion() );
+        registerExpansion( new HasPermissionExpansion() );
+        registerExpansion( new IsNearExpansion() );
+        registerExpansion( new NumberEqualsExpansion() );
+        registerExpansion( new NumberGreaterThanExpansion() );
+        registerExpansion( new NumberGreaterThanOrEqualsExpansion() );
+        registerExpansion( new NumberLessThanExpansion() );
+        registerExpansion( new NumberLessThanOrEqualsExpansion() );
+        registerExpansion( new RegexMatchesExpansion() );
+        registerExpansion( new StringContainsExpansion() );
+        registerExpansion( new StringEndsWithExpansion() );
+        registerExpansion( new StringEqualsExpansion() );
+        registerExpansion( new StringEqualsIgnoreCaseExpansion() );
+        registerExpansion( new StringStartWithExpansion() );
+
+        requirementExpansions.put( "" , new StringStartWithExpansion() );
+
+    }
+
+    public void reloadManager() {
+
+    }
+
+    public JavaPlugin getJavaPlugin() {
+        return javaPlugin;
+    }
+
+    public Economy getEconomy() {
+        return economy;
+    }
+
+    public HashMap<String, Requirements> getRequirements() {
+        return requirements;
+    }
+
+    public HashMap<String, RequirementExpansion> getRequirementExpansions() {
+        return requirementExpansions;
+    }
+
+    public void registerExpansion( RequirementExpansion requirementExpansion ) {
+
+        requirementExpansions.put( requirementExpansion.getType() , requirementExpansion );
+
+    }
+
+    public Requirements loadRequirements( ConfigurationSection configurationSection ) {
+
+        Requirements requirements = new Requirements();
+
+        for ( String name : configurationSection.getKeys(false) ) {
+
+            Map< String , Object > format = configurationSection.getConfigurationSection( name ).getValues( true );
+
+            Requirement requirement = loadRequirement( name , format );
+
+            if ( requirement == null) continue;
+
+            requirements.addRequirement( requirement );
+
+        }
+
+        return requirements;
+
+    }
+
+    public Requirement loadRequirement( String name , Map< String , Object > format ) {
+
+        Requirement requirement = null;
+
+        if ( format.containsKey( "type" ) ) {
+
+            String type = ( String ) format.get( "type" );
+
+            if ( type.startsWith( "!" ) ) type = type.replaceFirst( "!" , "");
+
+            if ( requirementExpansions.containsKey( type ) ) {
+
+                requirement = requirementExpansions.get( type ).generateRequirement( name , format );
+
+            }
+
+        }
+
+        if ( requirement != null ) requirement.setRequirementManager( this );
+
+        return requirement;
+
+    }
+
+}

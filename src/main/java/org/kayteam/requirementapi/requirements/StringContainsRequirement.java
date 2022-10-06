0139d17903f12c043ea6fff36cad47cb3a3641da
SoyAldo <pereyraosvaldo@live.com.ar>
2022-10-06 16:44:17 -0300

    StringContains implemented.

---
+++ src/main/java/org/kayteam/requirementapi/requirements/StringContainsRequirement.java
@@ -0,0 +1,55 @@
+package org.kayteam.requirementapi.requirements;
+
+import org.bukkit.entity.Player;
+import org.kayteam.requirementapi.Requirement;
+import org.kayteam.requirementapi.util.PlaceholderUtil;
+
+import java.util.LinkedHashMap;
+
+public class StringContainsRequirement extends Requirement {
+
+    private final String input , output;
+
+    public StringContainsRequirement( String name , String input , String output , boolean positive ) {
+        super( name , "string contains" , positive );
+        this.input = input;
+        this.output = output;
+    }
+
+    public String getInput() {
+        return input;
+    }
+
+    public String getOutput() {
+        return output;
+    }
+
+    @Override
+    public boolean verify( Player player ) {
+
+        String realInput = input , realOutput = output;
+
+        realInput = PlaceholderUtil.setPlaceholders( player , realInput);
+
+        realOutput = PlaceholderUtil.setPlaceholders( player , realOutput);
+
+        return isPositive() == ( realInput.contains( realOutput ) );
+
+    }
+
+    @Override
+    public LinkedHashMap<String, Object> serialize() {
+
+        LinkedHashMap<String, Object> result = super.serialize();
+
+        if ( ! isPositive() ) result.put( "type" , "!string contains" );
+
+        result.put( "input" , input );
+
+        result.put( "output" , output );
+
+        return result;
+
+    }
+
+}

ba68acafd02f4af9a5ba8233942fcecc4baafa32
SoyAldo <pereyraosvaldo@live.com.ar>
2022-10-06 16:41:06 -0300

    RequirementExpansion.java created

---
+++ src/main/java/org/kayteam/requirementapi/RequirementExpansion.java
@@ -0,0 +1,19 @@
+package org.kayteam.requirementapi;
+
+import java.util.Map;
+
+public abstract class RequirementExpansion {
+
+    private final String type;
+
+    public RequirementExpansion(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public abstract Requirement generateRequirement(String name , Map< String , Object > format );
+
+}

2f693c1669b27bc300b48913951cdab30c9a1427
SoyAldo <pereyraosvaldo@live.com.ar>
2022-10-06 16:43:22 -0300

    NumberGreaterThanOrEquals implemented.

---
+++ src/main/java/org/kayteam/requirementapi/requirements/NumberGreaterThanRequirement.java
@@ -0,0 +1,65 @@
+package org.kayteam.requirementapi.requirements;
+
+import org.bukkit.entity.Player;
+import org.kayteam.requirementapi.Requirement;
+import org.kayteam.requirementapi.util.PlaceholderUtil;
+
+import java.util.LinkedHashMap;
+
+public class NumberGreaterThanRequirement extends Requirement {
+
+    private final String input , output;
+
+    public NumberGreaterThanRequirement(String name , String input , String output , boolean positive ) {
+        super( name , "number greater than" , positive );
+        this.input = input;
+        this.output = output;
+    }
+
+    public String getInput() {
+        return input;
+    }
+
+    public String getOutput() {
+        return output;
+    }
+
+    @Override
+    public boolean verify( Player player ) {
+
+        String realInput = input , realOutput = output;
+
+        realInput = PlaceholderUtil.setPlaceholders( player , realInput);
+
+        realOutput = PlaceholderUtil.setPlaceholders( player , realOutput);
+
+        try {
+
+            int parsedInput = Integer.parseInt( realInput );
+
+            int parsedOutput = Integer.parseInt( realOutput );
+
+            return isPositive() == ( parsedInput > parsedOutput );
+
+        } catch ( NumberFormatException ignore ) {}
+
+        return !isPositive();
+
+    }
+
+    @Override
+    public LinkedHashMap<String, Object> serialize() {
+
+        LinkedHashMap<String, Object> result = super.serialize();
+
+        if ( ! isPositive() ) result.put( "type" , "!number greater than" );
+
+        result.put( "input" , input );
+
+        result.put( "output" , output );
+
+        return result;
+
+    }
+
+}

1b27880107df3db1bdfbe84eac8a1d99fd9eef35
SoyAldo <pereyraosvaldo@live.com.ar>
2025-10-01 12:15:22 -0300

    [update] New methods added on RequirementUtil.java

--- src/main/java/me/soyaldo/requirementapi/util/RequirementUtil.java
+++ src/main/java/me/soyaldo/requirementapi/util/RequirementUtil.java
@@ -1,5 +1,10 @@
 package me.soyaldo.requirementapi.util;
 
+import me.soyaldo.actionapi.util.ColorUtil;
+import me.soyaldo.actionapi.util.PapiUtil;
+import me.soyaldo.actionapi.util.TextUtil;
+import org.bukkit.entity.Player;
+
 import java.util.Map;
 
 public class RequirementUtil {
@@ -18,6 +23,41 @@ public class RequirementUtil {
             }
         }
         return true;
+    }
+
+    /**
+     * Apply replacements and PlaceholdersAPI on the text
+     *
+     * @param text         The text to process
+     * @param player       The player to apply PlaceholdersAPI
+     * @param replacements The replacements
+     * @param color        If apply color on the text
+     * @return The text modified
+     */
+    public static String processText(String text, Player player, String[][] replacements, boolean color) {
+        String modifiedText = text;
+        // => Apply the replacement
+        modifiedText = TextUtil.replace(modifiedText, replacements);
+        // => Apply PlaceholderAPI
+        modifiedText = PapiUtil.setPlaceholders(player, modifiedText);
+        // => Apply color
+        if (color) {
+            modifiedText = ColorUtil.colorizeLegacy(modifiedText);
+        }
+        // => Returning the modified text
+        return modifiedText;
+    }
+
+    /**
+     * Apply replacements and PlaceholdersAPI on the text without color
+     *
+     * @param text         The text to process
+     * @param player       The player to apply PlaceholdersAPI
+     * @param replacements The replacements
+     * @return The text modified
+     */
+    public static String processText(String text, Player player, String[][] replacements) {
+        return processText(text, player, replacements, false);
     }
 
 }

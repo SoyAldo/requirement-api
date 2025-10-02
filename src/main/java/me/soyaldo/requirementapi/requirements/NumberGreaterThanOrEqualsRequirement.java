8b9d0de5d7182b85f19ad968254b3875df22c405
SoyAldo <pereyraosvaldo@live.com.ar>
2025-10-02 19:14:50 -0300

    [update] NumberGreaterThanOrEqualsRequirement.java refactorized

--- src/main/java/me/soyaldo/requirementapi/requirements/NumberGreaterThanOrEqualsRequirement.java
+++ src/main/java/me/soyaldo/requirementapi/requirements/NumberGreaterThanOrEqualsRequirement.java
@@ -2,13 +2,15 @@ package me.soyaldo.requirementapi.requirements;
 
 import me.soyaldo.requirementapi.Requirement;
 import me.soyaldo.requirementapi.util.PlaceholderApi;
+import me.soyaldo.requirementapi.util.RequirementUtil;
 import org.bukkit.entity.Player;
 
 import java.util.LinkedHashMap;
 
 public class NumberGreaterThanOrEqualsRequirement extends Requirement {
 
-    private final String input, output;
+    private final String input;
+    private final String output;
 
     public NumberGreaterThanOrEqualsRequirement(String name, String input, String output, boolean positive) {
         super(name, "number greater than or equals", positive);
@@ -26,24 +28,19 @@ public class NumberGreaterThanOrEqualsRequirement extends Requirement {
 
     @Override
     public boolean onVerify(Player player, String[][] replacements) {
-        String realInput = input;
-        String realOutput = output;
-
-        for (String[] replacement : replacements) {
-            realInput = realInput.replace(replacement[0], replacement[1]);
-            realOutput = realOutput.replace(replacement[0], replacement[1]);
-        }
-
-        realInput = PlaceholderApi.setPlaceholders(player, realInput);
-        realOutput = PlaceholderApi.setPlaceholders(player, realOutput);
-
+        String realInput = RequirementUtil.processText(input, player, replacements);
+        String realOutput = RequirementUtil.processText(output, player, replacements);
+        // => Trying to parse the input and output
         try {
+            // Parsing the input and output
             int parsedInput = Integer.parseInt(realInput);
             int parsedOutput = Integer.parseInt(realOutput);
+            // Return the result
             return isPositive() == (parsedInput >= parsedOutput);
         } catch (NumberFormatException ignore) {
+            // If any is wrong return false
+            return false;
         }
-        return !isPositive();
     }
 
     @Override
